af396f7d4748bf1db07f604c40b5ee6c3605dd2f
Gustavo Martins Pereira <gustavo.pereira@etaure.com.br>
2022-06-06 13:17:50 -0300

    ADD - Class/Style Binding

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -7,16 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-	url: string = "http://loiane.com";
-	urlImage: string = "http://lorempixel.com.br/800/600/?2";
-
 	constructor() {	}
 
 	ngOnInit(): void {
-	}
-
-	getValue() {
-		return 1;
 	}
 
 }
